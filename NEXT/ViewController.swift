890e838991467d16c4b6614c56b31ecca6304909
Viktor Bán <viktor.ban@hiflylabs.com>
2022-02-22 16:27:33 +0100

    Initial Commit

---
+++ NEXT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NEXT
+//
+//  Created by Ban_Viktor on 2022. 02. 22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
